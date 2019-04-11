998f8d4e52798fdf1a15ba64f6a1614925e05a36
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-11 13:01:57 -0300

    Adicionando classe arquivo e sua implementação inicial

---
+++ Escalonador de Processos/Arquivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Escalonador_de_Processos
+{
+    class Arquivo
+    {
+        // Formatação do arquivo
+        // PID; Nome; Prioridade; QtdeCiclos; TempoCPU (talvez)
+
+        public static Escalonador LeituraArquivo(string nomeArquivo)
+        {
+            Escalonador auxiliar = new Escalonador();
+
+            //Fazer a leitura do arquivo e organizar entre as 10 listas Circulares
+            StreamReader arquivoLeitura = new StreamReader(nomeArquivo);
+
+
+
+            arquivoLeitura.Close();
+
+            return auxiliar;
+        }
+    }
+}
