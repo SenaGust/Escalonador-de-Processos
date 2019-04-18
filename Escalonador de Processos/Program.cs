fbb9b0127699c6e779e232b90bef5f656985ad6f
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-18 04:05:36 -0300

    Retirando testar

--- Escalonador de Processos/Program.cs
+++ Escalonador de Processos/Program.cs
@@ -10,7 +10,7 @@ namespace Escalonador_de_Processos
     {
         static void Main(string[] args)
         {
-            //testar();
+            testar();
 
             string nomeArquivo = "teste.txt";
             Escalonador escalonador = Arquivo.LeituraArquivo(nomeArquivo);
@@ -28,25 +28,6 @@ namespace Escalonador_de_Processos
             //Fim
             Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
-        }
-        static void testar()
-        {
-            Processos processos1 = new Processos(1, "gugu", 1 , 1);
-            Processos processos2 = new Processos(2, "luna", 1 , 2);
-            Processos processos3 = new Processos(3, "julinha", 1 , 3);
-            Processos processos4 = new Processos(4, "tavinho", 1 , 4);
-            Processos processos5 = new Processos(5, "Alice", 1 , 5);
-
-            ListaCircular escalonador = new ListaCircular();
-
-            escalonador.Inserir(processos1);
-            escalonador.Inserir(processos2);
-            escalonador.Inserir(processos3);
-            escalonador.Inserir(processos4);
-            escalonador.Inserir(processos5);
-
-            Console.WriteLine(escalonador.ToString());
-
         }
     }
 } //teste
