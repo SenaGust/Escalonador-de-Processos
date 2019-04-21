d8b621f71d9ab81967579e1287974da564a6175d
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-21 13:23:10 -0300

    Criação de dois métodos, um deles contém um teste e o outro a implementação "final"

--- Escalonador de Processos/Program.cs
+++ Escalonador de Processos/Program.cs
@@ -10,6 +10,48 @@ namespace Escalonador_de_Processos
     {
         static void Main(string[] args)
         {
+            //testar();
+            ProgramFinal();
+
+            //Fim
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        static void testar()
+        {
+            ListaCircular listaCircular = new ListaCircular();
+
+            Processos A = new Processos(1, "Primeiro", 1, 1);
+            Processos B = new Processos(2, "Segundo", 1, 1);
+            Processos C = new Processos(3, "Terceiro", 1, 1);
+            Processos D = new Processos(4, "Quarto", 1, 1);
+            Processos E = new Processos(5, "Quinto", 1, 1);
+            Processos F = new Processos(6, "Sexto", 1, 1);
+
+            listaCircular.Inserir(A);
+            listaCircular.Inserir(B);
+            listaCircular.Inserir(C);
+            listaCircular.Inserir(D);
+            listaCircular.Inserir(E);
+            listaCircular.Inserir(F);
+
+            Console.WriteLine("Antes de retirar");
+            Console.WriteLine(listaCircular.ToString());
+
+            while(!listaCircular.Vazia())
+            {
+                Console.WriteLine("Retirou: " + listaCircular.Retirar());
+                Console.WriteLine("Retirou: " + listaCircular.Retirar());
+                Console.WriteLine(listaCircular.ToString());
+            }
+
+            Console.WriteLine("Vazia");
+            Console.WriteLine(listaCircular.ToString());
+        }
+
+        static void ProgramFinal()
+        {
             string nomeArquivo = "teste.txt";
             Escalonador escalonador = Arquivo.LeituraArquivo(nomeArquivo);
 
@@ -18,14 +60,10 @@ namespace Escalonador_de_Processos
             else
             {
                 Console.WriteLine(escalonador.ToString());
-                escalonador.Run();
+                escalonador.Run(10);
             }
 
             Console.WriteLine(escalonador.ToString());
-
-            //Fim
-            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
-            Console.ReadKey();
         }
     }
 } //teste
