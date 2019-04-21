d36f7af268614e67c7b3ba8ca4e52cc72b16665c
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-21 13:17:42 -0300

    Implementação da classe

---
+++ Escalonador de Processos/Fila.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escalonador_de_Processos
+{
+    class Fila : IEstruturaDados
+    {
+        #region Atributos
+        public Elemento Primeiro { get; set; }
+        public Elemento Ultimo { get; set; }
+        #endregion
+
+        #region Construtor
+        public Fila()
+        {
+            this.Ultimo = this.Primeiro = new Elemento(null); //Criação do Sentinela
+        }
+        #endregion
+
+        #region Métodos
+        public void Inserir(IDado dado)
+        {
+            Ultimo = Ultimo.Proximo = new Elemento(dado);
+        }
+        public IDado Retirar()
+        {
+            if (Vazia())
+                return null;
+
+            Elemento auxRetirada = this.Primeiro.Proximo;
+            this.Primeiro.Proximo = auxRetirada.Proximo;
+
+            if (auxRetirada.Proximo != null)
+                auxRetirada.Proximo = null;
+            else
+                this.Ultimo = this.Primeiro;
+
+            return auxRetirada.MeuDado;
+        }
+        public override string ToString()
+        {
+            StringBuilder auxImpressao = new StringBuilder();
+            Elemento aux = Primeiro;
+
+            while(aux.Proximo != null)
+            {
+                auxImpressao.AppendLine(aux.Proximo.MeuDado.ToString());
+                aux = aux.Proximo;
+            }
+
+            return auxImpressao.ToString();
+        }
+        public bool Vazia()
+        {
+            return Primeiro == Ultimo;
+        }
+        #endregion
+    }
+}
